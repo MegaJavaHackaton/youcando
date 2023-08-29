4403a08c2b0734244e24a29b646ab74e66bafd86
chxni <jcg3098@naver.com>
2023-08-29 15:59:20 +0900

    init

---
+++ src/test/java/com/megajava/youcando/YoucandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megajava.youcando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoucandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
